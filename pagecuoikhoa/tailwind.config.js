50c6e33773fc2fd3cac12aea9d3d65bfb0b86486
Thao <ntthao460@gmail.com>
2021-10-16 20:06:13 +0700

    Update

--- pagecuoikhoa/tailwind.config.js
+++ pagecuoikhoa/tailwind.config.js
@@ -6,10 +6,27 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
-      center: true
+      center: true,
+      padding: '2rem',
     },
     fontFamily: {
       'body': ["'Inter', sans-serif;"]
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
 
     extend: {
